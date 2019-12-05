d4fbdddfa9f19d5f855ffd8415e398ba52d73ce8
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-05 22:07:44 +0100

    1.20191205.4

--- CSModulModulTablaHozzarendelo/MainWindow.xaml.cs
+++ CSModulModulTablaHozzarendelo/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿
 using CSLibAc4yObjectDBCap;
 using CSLibAc4yObjectObjectService;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -31,6 +30,10 @@ namespace CSModulModulTablaHozzarendelo
                 new Ac4yObjectObjectService(conn).ListInstanceByName(
                     new ListInstanceByNameRequest() { TemplateName = "Tábla" }
                 );
+            ListInstanceByNameResponse listInstanceByNameResponseModul =
+                new Ac4yObjectObjectService(conn).ListInstanceByName(
+                    new ListInstanceByNameRequest() { TemplateName = "Modul" }
+                );
 
             Ac4yObjectHomeList = listInstanceByNameResponse.Ac4yObjectHomeList;
             foreach (Ac4yObjectHome home in Ac4yObjectHomeList)
@@ -38,6 +41,10 @@ namespace CSModulModulTablaHozzarendelo
                 zoneList.Add(home.HumanId);
             }
             lbOne.ItemsSource = zoneList;
+            foreach (var modul in listInstanceByNameResponseModul.Ac4yObjectHomeList)
+            {
+                cBox.Items.Add(modul.HumanId);
+            }
         }
 
         private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -50,6 +57,28 @@ namespace CSModulModulTablaHozzarendelo
             {
                 DragDrop.DoDragDrop(parent, data, DragDropEffects.Move);
             }
+        }
+
+        private void Upload(object sender, RoutedEventArgs e)
+        {
+            
+            string modul = cBox.SelectedItem.ToString();
+            List<string> tablak = new List<string>();
+
+            for (int i = 0; i < lbTwo.Items.Count; i++)
+            {
+                ListBoxItem lb = (ListBoxItem)lbTwo.ItemContainerGenerator.ContainerFromIndex(i);
+                tablak.Add(lb.Content.ToString());
+            }
+
+            foreach(var tabla in tablak)
+            {
+                SetByNamesResponse setByNamesResponse =
+                    new Ac4yObjectObjectService(conn).SetByNames(
+                        new SetByNamesRequest() { TemplateName = modul, Name = tabla }
+                    );
+            }
+
         }
 
         private void ListBox_Drop(object sender, DragEventArgs e)
